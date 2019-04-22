75334ee378bdbdaac1ebcf5392c9d68fb250b222
Will McElhenney <whm0004@auburn.edu>
2019-04-22 15:05:14 -0500

    Safety1

--- Code/fixFASTA.py
+++ Code/fixFASTA.py
@@ -1,16 +1,25 @@
+#script for fixing copied and pasted flatfile sequences
 import re
+from sys import argv
 
-re.compile("[0-9]+")
-
-file = "C:\\Users\\whm0004\\Google Drive\\FinalProject\\Fcamr gene - AB071978"
-
+file = argv[1]
+identifier = ">" + argv[2] + "\n"
 data = ""
 
 with open(file) as f:
-    data = f.read()
-    data = re.sub("[0-9]+", '', data)
-    data = re.sub("\s", '', data)
+	data = f.read()
+	data = re.sub("[0-9]+",'',data)
+	data = re.sub("\s",'',data)
 
-with open(file, 'w') as f:
-    f.write(">AB071978\n")
-    f.write(data)
+with open(file,'w') as f:
+	f.write(identifier)
+
+	i = 0
+	while i < len(data):
+		if len(data[i:]) > 80:
+			f.write(data[i:i+80] + '\n')
+			i += 80
+
+		else:
+			f.write(data[i:] + '\n')
+			i = len(data)
